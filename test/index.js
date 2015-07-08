9469c8062fb7bfdef33004376ab264c45965d62f
Gaohaoyang <gaohaoyang126@126.com>
2015-07-08 16:08:06 +0800

    add

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-var a = 0;
+/*var a = 0;
 var b = 0;
 
 function A(a) {
@@ -8,8 +8,36 @@ function A(a) {
     alert(b);
 }
 A(1); //0
-B(1); //error
+B(1); //error*/
 /*这是联想前端的一道笔试题，我记不太清楚，好像是这样*/
 // 还有一些我没答好的
 // Ajax跨域
 // JavaScript类，继承，私有变量
+// 
+
+
+var str = '"[1,2,3]"';
+var arrStr = str.slice(1,length-1);
+var array = JSON.parse(arrStr);
+console.log(arrStr);
+console.log(arrStr[0]);
+console.log(array[0]);
+
+var test = function() {
+    console.log("函数表达式");
+};
+
+test();
+
+
+
+/*function test() {
+    alert('test');
+};
+var aa = test;
+test = function() {
+    alert('test1');
+};
+
+aa();
+test();*/
