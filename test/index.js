f20efe67c238f47384fbe643bca6f41741e958b1
Gaohaoyang <gaohaoyang126@126.com>
2015-09-17 10:11:37 +0800

    update

--- test/index.js
+++ test/index.js
@@ -1,3 +1,107 @@
+
+
+/*
+ * 程序分析：对n进行分解质因数，应先找到一个最小的质数k，然后按下述步骤完成：
+ * (1)如果这个质数恰等于n，则说明分解质因数的过程已经结束，打印出即可。
+ * (2)如果n>=k，但n能被k整除，则应打印出k的值，并用n除以k的商,作为新的正整数你n,重复执行第一步。&nbsp;&nbsp;&nbsp;
+ * (3)如果n不能被k整除，则用k+1作为k的值,重复执行第一步。&nbsp;&nbsp;
+ */
+/*    public static void primeFactor() {// 该方法将一个正整数分解成质因数相乘的形式
+
+        Scanner scan = new Scanner(System.in);// 接收控制台输入的信息
+        System.out.print("请输入一个正整数：");
+
+        try {
+            int num = scan.nextInt();// 取出控制台输入的信息
+
+            if (num < 2) {// 若输入的数小于2,输出提示信息
+
+                System.out.println("必须输入不小于2的正整数！");
+            } else {
+
+                int primeNumber = 2;// 定义最小的质数
+                System.out.print(num + " = ");
+                
+                while (primeNumber <= num) {// 在质数小于输入的数时，进行循环
+
+                    if (primeNumber == num) {// 当质数等于输入的数时,直接输出
+
+                        System.out.print(num);
+                        break;// 跳出循环
+
+                    } else if (num % primeNumber == 0) {// 当输入的数与质数的余数为0时,输出这个质数
+
+                        System.out.print(primeNumber + " * ");
+                        num = num / primeNumber;// 把剩下的结果赋给num
+                        
+                    } else {// 在余数不为0时,质数递增
+                        primeNumber++;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("必须输入正整数！");// 捕捉异常,输出必须输入整数
+        }
+    }
+}*/
+
+
+
+
+
+
+
+
+
+/*var line;
+while (line = readline()) {
+    line = line.split(' ');
+    if(line.length>1){
+        cost(parseInt(line[0]),parseInt(line[1]),parseInt(line[2]),parseInt(line[3]));
+    }
+}
+
+
+function cost(z, e, m, s) {
+    var sum = z + e + m + s;
+    if (z >= 60 && e >= 60 && m >= 90 && s >= 90) {
+        if (sum >= 350) {
+            print('Gongfei');
+        } else if (sum >= 310) {
+            print('Zifei');
+        } else {
+            print('Fail');
+        }
+    } else {
+        print('Fail');
+    }
+}
+
+cost(61,62,100,120);
+cost(80,80,120,100);
+cost(55,90,130,130);
+*/
+
+
+// for (var c = 0; c < 100; c++) {
+//     for (var r = 0; r < 100; r++) {
+//         for (var j = 0; j < 100; j++) {
+//             for (var f = 0; f < 100; f++) {
+//                 if (c + 5 == r - 5 && r - 5 == 5 * j && 5 * j == f / 5 && f / 5 == c + 5) {
+//                     console.log(c);
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// var a = 123456789012345678901234567890;
+// var b = 345;
+
+// console.log(a/b);
+
+
+
 /*var a = 0;
 var b = 0;
 
@@ -50,4 +154,4 @@ test();*/
     var x;
     console.log(x);
 }
-foo4(); // funtion x()*/
+foo4(); // funtion x()
---
+++ test/testMi.js
@@ -0,0 +1,2 @@
+console.log('mi');
+// print('aaaaa');
